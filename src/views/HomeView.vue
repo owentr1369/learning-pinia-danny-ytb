c4b488d5ada74f7b78939d3a44a00d1297c6d5ee
TranTam <tamvaa1306@gmail.com>
2022-09-24 19:46:18 +0700

    data & methods

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,36 @@
+<template>
+  <div class="home">
+    <div class="count">{{ count }}</div>
+    <div class="buttons">
+      <button @click="decreaseCount">Decrease</button>
+      <button @click="increaseCount">Increase</button>
+    </div>
+  </div>
+</template>
+
 <script setup>
+import { ref } from "vue";
+
+const count = ref(1);
+
+const decreaseCount = () => {
+  count.value--;
+};
+
+const increaseCount = () => {
+  count.value++;
+};
 </script>
 
-<template>
-  <main>
-    <h1>This is an home page</h1>
-  </main>
-</template>
+
+<style>
+.count {
+  font-size: 60px;
+  margin: 20px;
+}
+.buttons button {
+  font-size: 40px;
+  margin: 10px;
+  border-radius: 4px;
+}
+</style>
